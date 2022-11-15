2f75026eae715bb48f49258f77b19c23793fa51b
zaid ragab <zragab@lcs.on.ca>
2022-11-15 14:41:04 -0500

    Initial Commit

---
+++ StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by Zaid Ragab on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
